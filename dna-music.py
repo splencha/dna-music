304226b7825d4ff302a34ee174f2edc271ffefec
splencha <blinddonkeyhotai@gmail.com>
2012-10-27 12:54:45 -0400

    first py code

---
+++ dna-music.py
@@ -0,0 +1,21 @@
+import fileinput
+
+""" The initial procedure to_codons() converts a text file to
+lists of codons (as strings) and initializers as integers.
+"""
+
+
+def to_codons(prompt,species = 'C. elegans',style = 'Baroque 3-part with continuo'):
+    spp_text = 'The species selected for conversion to music is:'
+    style_text = 'The musical style selected is:'
+    err_text = 'Dna-music cannot proceed. Program will exit now.'
+    if os.path.exists(/home/david/Documents/Genomes/C_elegans_chr-001.txt):
+        raw_text = open(/home/david/Documents/Genomes/C_elegans_chr-001.txt,rU,1)
+        print spp_text + species + '\n'
+        print style_text + style
+    elif:
+	    print "No file exists at the specified location." + '\n'
+        print err_text
+    
+    
+    
